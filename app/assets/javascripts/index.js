58278da6eeea201109d90819046cb6ef722dd480
Dylan Richards <dylanerichards@gmail.com>
2015-05-20 14:47:57 -0400

    Deal with fade-ins

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
+
+  setTimeout(function() {
+  // $(".logo").show().addClass("animated fadeInLeft");
+
+    $(".right-panel").show().addClass("animated fadeInRight");
+    $(".left-panel").show().addClass("animated fadeInLeft");
+    $(".close").css("visibility", "visible").addClass("animated fadeInRight");
+
+  }, 1000)
+
+  setTimeout(function() {
+    $(".logo").show().addClass("animated fadeInLeft");
+  }, 1300)
 });
